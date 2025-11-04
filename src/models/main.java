c46baa7566fad03cfa8f7f479f2bf73c16d30959
Naedson-Vilela <naedsondavi@gmail.com>
2025-11-04 18:36:32 -0300

    cod base

---
+++ src/models/main.java
@@ -0,0 +1,12 @@
+import src.model.Pessoa;
+
+public class main {
+    Pessoa pessoa = new Pessoa();
+    pessoa.nome = "Naedson";
+    pessoa
+
+
+    public static void main(String[] args) {
+        
+    }
+}
